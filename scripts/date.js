bd43ead3efaea77b8f20eb8e91322b082c4f8818
emma-2982 <eteye@byupathway.edu>
2026-03-10 15:57:15 +0400

    Course home page

---
+++ scripts/date.js
@@ -0,0 +1,5 @@
+document.querySelector('#lastModified').textContent = document.lastModified;
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+document.querySelector("#lastModified").textContent =
+    "Last Modified: " + document.lastModified;
