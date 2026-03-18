43dc3714dbdbe4857fe1987ae50dc6af4cc3e121
emma-2982 <eteye@byupathway.edu>
2026-03-18 16:41:29 +0400

    final directory page

--- chamber/scripts/cham.js
+++ chamber/scripts/cham.js
@@ -1,1 +1,16 @@
 
+// MOBILE MENU TOGGLE
+const menuButton = document.querySelector("#menu-toggle");
+const navList = document.querySelector("#primary-nav ul");
+
+menuButton.addEventListener("click", () => {
+    navList.classList.toggle("open");
+});
+
+// FOOTER DATES
+document.getElementById("current-year").textContent = new Date().getFullYear();
+document.getElementById("last-modified").textContent = document.lastModified;
+
+// STUDENT NAME
+document.getElementById("student-name").textContent = "Emmanuel";
+
